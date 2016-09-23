1db38c50d229229ea7eed095884db356b14ed404
AriderM <YOUbet5078>
2016-09-23 13:05:12 -0400

    Updating to add PHP

--- submit.php
+++ submit.php
@@ -1,9 +1,22 @@
+<?php
+	echo "<p>Request</p>";
+	echo "<pre>";
+	print_r($_REQUEST);
+	echo "<pre>";
+	
+	echo "<p>Files</p>";
+	echo "<pre>";
+	print_r($_FILES);
+	echo "<pre>";
+?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Payments</title>
+	<title>Thank You!</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
